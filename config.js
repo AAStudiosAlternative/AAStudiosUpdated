80eafedebf7b650c36429f42b042cdcc34b6b7a9
AAStudiosAlternative <131726331+AAStudiosAlternative@users.noreply.github.com>
2024-07-10 14:56:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,39 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true,
   users: {
-    // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
-  },
-};
-export default config;
+    // username: 'password', you can add multiple users.
+    admin: 'whocares',
+    enigma: 'eightsix',
+    Sunscreen: 'MoNkEy123',
+    TheTrainGuy: '0523',
+    AshG: 'yourgay1',
+    mrreaper: 'reaper14127',
+    godzilla: 'brayden12',
+    osman: 'istanbul',
+   Moises12: 'Moises12',
+    poopenfart: 'Ikilledamanbehinda711',
+    SlimJim: 'JimJim33',
+    Manny: 'tisd11112009',
+    Glitch: '13775413',
+    Zambo: 'capybara',
+    mama: 'dadamama',
+    bdanbrown: 'soldier2016',
+    DogEater: '468101214',
+     Stitch740: 'flower98',
+     Watermelon: 'Munchers',
+     coolone: '12345678',
+     hearts4r4e: 'htxr4eee',
+     molotovs:'tisd06072010',
+    WalmartMan: 'friedchickenamerica',
+    CamPlayz10: 'Tisd09152009',
+    Jas: 'klklklkl',
+    P0okie: 'L0vecats',
+    Shaq: '12345678',
+    Nikki: 'neeBondha',
+    Monkeynotused: 'Paris',
+    gnatnotused: 'Saigon',
+    Deutschnotused: 'Amsterdam',
+
+  }
+}
+export default config
